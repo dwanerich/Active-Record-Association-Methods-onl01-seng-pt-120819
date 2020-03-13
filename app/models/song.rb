4b9c55778af3b7e9b8517d2c6acc3f65d89dab26
dwane richards <dwanerich@gmail.com>
2020-03-13 21:15:56 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -12,8 +12,6 @@ class Song < ActiveRecord::Base
     # Hint: you won't want to create an artist record every time this method is called, only if an Drake is *not found*
     
     
-    
-    drake = song.artist
     Artist.create(name: "Drake")
     drake = song.artist
   end
