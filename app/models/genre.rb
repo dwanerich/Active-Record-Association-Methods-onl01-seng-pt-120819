f24958308c0e1c1a915476b78d1b75b0e30fef08
dwane richards <dwanerich@gmail.com>
2020-03-13 20:59:35 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,5 +14,5 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-   self.artists.map { |artist| artist{:name} }
+   self.artists.map { |artist| artist[:name] }
 end
